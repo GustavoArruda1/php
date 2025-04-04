f7b37a147a0c425c9f4c69f1e340d75fe6072c4a
Gustavo Arruda <151965666+GustavoArruda1@users.noreply.github.com>
2025-04-04 17:43:56 -0300

    Update calculadora.php

--- calculadora.php
+++ calculadora.php
@@ -24,7 +24,7 @@
         if (!in_array($operacao, ['+', '-', '*', '/'])) {
             echo "Operação inválida, tente novamente! \n";
         } else {
-        $num1 = (float) readline ("Escolha o primeiro número: \n");
+        $num1 = (float) readline("Escolha o primeiro número: \n");
         $num2 = (float) readline("Escolha o segundo número: \n");
         if ($operacao == '+') {
             soma($num1, $num2);
@@ -47,57 +47,3 @@
     }
     
 ?>
-
-<?php/*
-
-function soma($num1, $num2) {
-    return $num1 + $num2;
-}
-
-function subtracao($num1, $num2) {
-    return $num1 - $num2;
-}
-
-function multiplicacao($num1, $num2) {
-    return $num1 * $num2;
-}
-
-function divisao($num1, $num2) {
-    if ($num2 == 0) {
-        return "Erro: divisão por zero!";
-    }
-    return $num1 / $num2;
-}
-
-while (true) {
-    $operacao = readline("Escolha a operação (+, -, *, /): ");
-
-    if (!in_array($operacao, ['+', '-', '*', '/'])) {
-        echo "Operação inválida, tente novamente!\n";
-        continue; 
-    }
-
-    $num1 = (float) readline("Escolha o primeiro número: ");
-    $num2 = (float) readline("Escolha o segundo número: ");
-
-    switch ($operacao) {
-        case '+':
-            $resultado = soma($num1, $num2);
-            break;
-        case '-':
-            $resultado = subtracao($num1, $num2);
-            break;
-        case '*':
-            $resultado = multiplicacao($num1, $num2);
-            break;
-        case '/':
-            $resultado = divisao($num1, $num2);
-            break;
-    }
-
-    echo "Seu resultado é: $resultado\n";
-    break;
-}
-
-?>*/
-
