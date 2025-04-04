d2463871a7eddd227d3bc1fa96cf5fb3ac11beac
Gustavo Arruda <151965666+GustavoArruda1@users.noreply.github.com>
2025-04-04 20:59:52 -0300

    Update calculadora.php

--- calculadora.php
+++ calculadora.php
@@ -1,49 +1,51 @@
 <?php
 
-    function soma($num1, $num2) {
-        return $num1 + $num2;
-    }
-    
-    function subtracao($num1, $num2) {
+function soma($num1, $num2) {
+    return $num1 + $num2;
+}
+
+function subtracao($num1, $num2) {
     return $num1 - $num2;
-    }
-    
-    function multiplicacao($num1, $num2) {
+}
+
+function multiplicacao($num1, $num2) {
     return $num1 * $num2;
-    }
-    
-    function divisao($num1, $num2) {
-    if ($num2 == 0) {
-        return "Erro: divisão por zero!";
+}
+
+function divisao($num1, $num2) {
+    if($num2 == 0) {
+        return 'Erro: Divisão por zero!';
     }
     return $num1 / $num2;
+}
+
+while(true) {
+    $operacao = readline("Escolha a operação desejada (+, -, *, /): \n");
+    if(!in_array($operacao, ['+', '-', '*', '/'])) {
+        echo "Operação inválida, tente novamente!\n";
+        continue;
     }
     
-    while(true) {
-        $operacao = readline("Escolha qual operação deseja (+, -, *, /): \n");
-        if (!in_array($operacao, ['+', '-', '*', '/'])) {
-            echo "Operação inválida, tente novamente! \n";
-        } else {
-        $num1 = (float) readline("Escolha o primeiro número: \n");
-        $num2 = (float) readline("Escolha o segundo número: \n");
-        if ($operacao == '+') {
-            soma($num1, $num2);
-            echo 'Seu resultado é '.soma($num1, $num2);
+    $num1 = (float) readline("Digite o primeiro número: \n");
+    $num2= (float) readline("Digite o segundo número: \n");
+    
+    switch($operacao) {
+        case '+':
+            $resultado = soma($num1, $num2);
             break;
-        } else if ($operacao == '-') {
-            subtracao($num1, $num2);
-            echo 'Seu resultado é '.subtracao($num1, $num2);
+        case '-':
+            $resultado = subtracao($num1, $num2);
             break;
-        } else if ($operacao == '*') {
-            multiplicacao($num1, $num2);
-            echo 'Seu resultado é: '. multiplicacao($num1, $num2);
+        case '*':
+            $resultado = multiplicacao($num1, $num2);
             break;
-        } else if ($operacao == '/') {
-            divisao($num1, $num2);
-            echo 'Seu resultado é: '. divisao($num1, $num2);
+        case '/':
+            $resultado = divisao($num1, $num2);
             break;
-        }
-    }
     }
     
+    echo 'Seu resultado é '. $resultado;
+    break;
+}
+
 ?>
